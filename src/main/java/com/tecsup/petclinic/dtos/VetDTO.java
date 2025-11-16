309f6d9faf12b1d53ba66d2d9bf0293ff4e8a851
Emerson Flores <emerson.flores.q@tecsup.edu.pe>
2025-11-15 22:32:41 -0500

    Subiendo proyecto

---
+++ src/main/java/com/tecsup/petclinic/dtos/VetDTO.java
@@ -0,0 +1,33 @@
+package com.tecsup.petclinic.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Data Transfer Object para Vet
+ * 
+ * @author jgomezm
+ *
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class VetDTO {
+
+	private Integer id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private String phone;
+	
+	private Boolean active;
+
+}
